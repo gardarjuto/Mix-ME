9eec7566557a0ae2a6dc6b60bab6feea1b8de9d6
gardarjuto <gi241@cam.ac.uk>
2023-06-26 00:15:35 +0100

    add grouping

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,11 +1,16 @@
 import wandb
+from wandb.util import generate_id
+import os
 
 
 def init_wandb(config):
+    os.environ["WANDB_RUN_GROUP"] = "experiment-" + generate_id()
+
     wandb.init(
-        project="multi-agent-quality-diversity",
+        project="MA-QD",
         config=config,
         save_code=True,
         name=config.get("experiment_name"),
         mode=config.get("wandb_mode"),
+        entity="ucl-dark",
     )
